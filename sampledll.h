ee5e8223bdd1318207e83252fa4077cafbceef8a
c17179n <64582595+c17179n@users.noreply.github.com>
2023-05-11 12:01:05 +0300

    Create sampledll.h

---
+++ sampledll.h
@@ -0,0 +1,14 @@
+typedef VOID (*DLLPROC) (LPTSTR);
+...
+HINSTANCE hinstDLL;
+DLLPROC HelloWorld;
+BOOL fFreeDLL;
+
+hinstDLL = LoadLibrary("sampleDLL.dll");
+if (hinstDLL != NULL)
+{
+    HelloWorld = (DLLPROC) GetProcAddress(hinstDLL, "HelloWorld");
+    if (HelloWorld != NULL)
+        (HelloWorld);
+    fFreeDLL = FreeLibrary(hinstDLL);
+}
