f9c3fb1e1529e088c0de2af0d58c8e59c4a688ab
vmc4ll <64582595+vmc4ll@users.noreply.github.com>
2023-06-24 14:12:17 +0300

    Update test driver.c

--- test driver.c
+++ test driver.c
@@ -1,6 +1,16 @@
-#include <ntddk.h>                  
 
-NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT driverObject, _In_ PUNICODE_STRING registryPath) {
-    KdPrint(("Hello World!\n"));    
+
+#include <ntddk.h>
+
+NTSTATUS DriverUnload(_In_ PDRIVER_OBJECT driverObject) {
+    KdPrint(("Goodbye World!\n"));
     return STATUS_SUCCESS;
 }
+
+NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT driverObject, _In_ PUNICODE_STRING registryPath) {
+    KdPrint(("Hello World!\n"));
+    driverObject->DriverUnload = DriverUnload; // Set the unload function to DriverUnload
+
+    return STATUS_SUCCESS;
+}
+
